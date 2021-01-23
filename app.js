be20aa3857e6eea5a442f34d2fa56ab4949a33b6
El Aguacate Programador <grilletjhony@gmail.com>
2021-01-22 23:10:26 -0400

    Configured by default with only one API url

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const BASE_URL = require('./utils').BASE_URL
-const writeJSON = require('./service').writeJSON
-const convertCurrencyFrom = require('./service').convertCurrencyFrom
+//const BASE_URL = require('./utils').BASE_URL
+//const writeJSON = require('./service').writeJSON
+//const convertCurrencyFrom = require('./service').convertCurrencyFrom
 const PORT = process.env.PORT
 
 const app = express()
 
 app.use(bodyParser.json())
 
-setTimeout(() => {
-  writeJSON()
-}, process.env.MARKET_API_TICK * 30 * 60 * 1000) // every half hour
+//setTimeout(() => {
+//  writeJSON()
+//}, process.env.MARKET_API_TICK * 30 * 60 * 1000) // every half hour
 
 /**
- * GET /chinchinapi/v1/
- * Get information about the API
+ * GET /
+ * Get price API
  */
-app.get(`${BASE_URL}`, (req, res, next) => {
-  res.send({ url: BASE_URL })
+app.get(`/`, (req, res, next) => {
+  res.send({ price: Math.round((Math.random() * 66000) + 65000) })
 })
 
 /**
  * GET /chinchinapi/v1/currencies
  * Get all registered currency market (Currently: BTC, DASH, ETH, EUR)
- */
+ 
 app.get(`${BASE_URL}/currencies`, async (req, res, next) => {
   try {
     const currencyList = require(`./${process.env.API_DB_FILENAME}`)
@@ -39,7 +39,7 @@ app.get(`${BASE_URL}/currencies`, async (req, res, next) => {
 /**
  * GET /chinchinapi/v1/convert/
  * Fallback URL in case of invalid parameters
- */
+ 
 app.get(`${BASE_URL}/convert/`, (req, res, next) => {
   res.send({'status': 400, 'message': '"amount" and "currency" params are mandatory'})
 })
@@ -47,7 +47,7 @@ app.get(`${BASE_URL}/convert/`, (req, res, next) => {
 /**
  * GET /chinchinapi/v1/convert/:amount/:currency
  * Converts the :amount of the :currency specified to all registered currencies
- */
+ 
 app.get(`${BASE_URL}/convert/:amount/:currency`, (req, res, next) => {
   try {
     const conversions = convertCurrencyFrom(
@@ -59,6 +59,7 @@ app.get(`${BASE_URL}/convert/:amount/:currency`, (req, res, next) => {
     res.send({ 'Error': err })
   }
 })
+*/
 
 app.listen(PORT, () => {
   console.log(`App (re)started on port ${PORT}`)
